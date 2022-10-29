1816f8f6b2413a3cac34cef723925950ccedee58
Shivareddykyadari <kyadarishivareddy1430@gmail.com>
2022-10-29 22:30:05 +0530

    react-state-management

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Messege from './Component/Messege'
+
+function App() {
+  return (
+    <div className="App">
+      <Messege/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Component/Messege.js
@@ -0,0 +1,29 @@
+
+import React ,{Component} from 'react'
+
+class Messege extends Component{
+
+    constructor(){
+        super()
+        this.state={
+            messege:"Welcome Visitor"
+        }
+    }
+    changeMessege(){
+        this.setState({
+            messege:'Thank you for Subscribing'
+        })
+    }
+
+
+    render(){
+        return(
+            <div>
+                <h1>{this.state.messege}</h1>
+                <button onClick={()=>this.changeMessege()}>Subscribe</button>
+            </div>
+        )
+    }
+}
+
+export default Messege;
